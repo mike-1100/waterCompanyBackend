c832f1705da782943a3d66f657d9228bb8193915
Almond <matric_number@veritas.edu.ng>
2026-02-04 21:13:55 +0100

    I don commit ooh

--- middleware/role.middleware.js
+++ middleware/role.middleware.js
@@ -1,8 +1,17 @@
-module.exports = (role) => {
-    return (req, res, next) => {
-        if (req.user.role.toLowerCase() !== role.toLowerCase()) {
-            return res.status(403).json({ error: "Access denied" });
-        }
-        next();
-    };
+
+module.exports = (...allowedRoles) => {
+  return (req, res, next) => {
+    if (!req.user || !req.user.role) {
+      return res.status(401).json({ error: "Unauthorized" });
+    }
+
+    const userRole = req.user.role.toUpperCase();
+
+    const roles = allowedRoles.map((r) => r.toUpperCase());
+
+    if (!roles.includes(userRole)) {
+      return res.status(403).json({ error: "Access denied" });
+    }
+    next();
+  };
 };
